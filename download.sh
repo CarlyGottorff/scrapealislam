01c39c796cfb07e2b7c648be14d1c77ab8b1cf18
Carly Gottorff <43753139+CarlyGottorff@users.noreply.github.com>
2022-01-05 20:12:26 -0500

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,7 +1,9 @@
+cd /home/cgottorff/scrapealislam/
+
 httrack --update https://www.alislam.org/press-release/ --user-agent msie6 --priority=p7
 
 httrack --update https://www.alislam.org/articles/ --user-agent msie6 --priority=p7
 
-/usr/bin/git add -A .
-/usr/bin/git commit -m "updates from download.sh"
-/usr/bin/git push
+git add -A .
+git commit -m "updates from download.sh"
+git push
