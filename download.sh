af028c566be13438fff8863dc74754b3675e7c80
Carly Gottorff <43753139+CarlyGottorff@users.noreply.github.com>
2022-01-10 10:05:24 -0800

    just articles

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 cd /home/cgottorff/scrapealislam/
 
-httrack --update https://www.alislam.org/articles/islams-principles-for-establishing-true-peace/ --user-agent msie6 --priority=p7
+httrack --update https://www.alislam.org/articles/ --user-agent msie6 --priority=p7
 
 git add -A .
 git commit -m "updates from download.sh"
