fbbf6d2fedc81824e92c1ed58a69f9b96faec8f4
Carly Gottorff <43753139+CarlyGottorff@users.noreply.github.com>
2022-01-10 16:25:45 -0800

    running articles and press releases
    running hatrack commands on same line

--- download.sh
+++ download.sh
@@ -1,7 +1,6 @@
 cd /home/cgottorff/scrapealislam/
 
-httrack --update https://www.alislam.org/articles/ --user-agent msie6 --priority=p7
-httrack --update https://www.alislam.org/press-release/ --user-agent msie6 --priority=p7
+httrack --update https://www.alislam.org/articles/, https://www.alislam.org/press-release/ --user-agent msie6 --priority=p7
 
 git add -A .
 git commit -m "updates from download.sh"
